691188e1ff54c2c0475365522f3adc6ad0cb6fd1
NA29 <nathan.audegond@hotmail.com>
2025-12-19 15:00:40 -0500

    adding breadcrumb winbar + integration with LSPs

--- init.lua
+++ init.lua
@@ -6,6 +6,9 @@ vim.cmd("set number")
 vim.cmd("set relativenumber")
 vim.g.mapleader = " "
 
+-- =========================
+-- lazy.nvim bootstrap
+-- =========================
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
   vim.fn.system({
@@ -18,6 +21,10 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
   })
 end
 vim.opt.rtp:prepend(lazypath)
+
+-- =========================
+-- Diagnostics UI
+-- =========================
 vim.diagnostic.config({
   float = {
     border = "rounded",
@@ -25,35 +32,39 @@ vim.diagnostic.config({
   severity_sort = true,
 })
 
+-- =========================
+-- Enable winbar (breadcrumbs)
+-- =========================
+vim.o.winbar = "%{%v:lua.require'barbecue.ui'.get_winbar()%}"
+
+-- =========================
+-- Plugins
+-- =========================
 local plugins = {
-  { 
-    "catppuccin/nvim", 
-    name = "catppuccin",  
+  {
+    "catppuccin/nvim",
+    name = "catppuccin",
     priority = 1000,
     lazy = false,
     config = function()
-      require("catppuccin").setup(
-        {
-          -- transparent_background = true,
-        }
-      )
-      vim.cmd.colorscheme "catppuccin"
-    end
+      require("catppuccin").setup({})
+      vim.cmd.colorscheme("catppuccin")
+    end,
   },
 
   {
-    'nvim-telescope/telescope.nvim', 
-    tag = '0.2.0',
-    dependencies = { 'nvim-lua/plenary.nvim' },
+    "nvim-telescope/telescope.nvim",
+    tag = "v0.2.0",
+    dependencies = { "nvim-lua/plenary.nvim" },
     config = function()
       local builtin = require("telescope.builtin")
-      vim.keymap.set('n', '<C-p>', builtin.find_files, {})
-      vim.keymap.set('n', '<leader>f', builtin.live_grep, {})
-    end
+      vim.keymap.set("n", "<C-p>", builtin.find_files, {})
+      vim.keymap.set("n", "<leader>f", builtin.live_grep, {})
+    end,
   },
 
   {
-    'nvim-treesitter/nvim-treesitter',
+    "nvim-treesitter/nvim-treesitter",
     build = ":TSUpdate",
     config = function()
       local status_ok, configs = pcall(require, "nvim-treesitter.configs")
@@ -61,115 +72,147 @@ local plugins = {
         return
       end
       configs.setup({
-        ensure_installed = { "lua", "javascript", "cpp", "python"},
+        ensure_installed = { "lua", "javascript", "cpp", "python" },
         sync_install = false,
         auto_install = true,
         highlight = { enable = true },
         indent = { enable = true },
       })
-    end
+    end,
   },
+
   {
-    'windwp/nvim-autopairs',
+    "windwp/nvim-autopairs",
     event = "InsertEnter",
     config = function()
       require("nvim-autopairs").setup({})
-    end
+    end,
   },
+
   {
     "nvim-neo-tree/neo-tree.nvim",
     branch = "v3.x",
     dependencies = {
       "nvim-lua/plenary.nvim",
       "MunifTanjim/nui.nvim",
-      "nvim-tree/nvim-web-devicons", -- optional, but recommended
+      "nvim-tree/nvim-web-devicons",
     },
-    lazy = false, -- neo-tree will lazily load itself
-
+    lazy = false,
     config = function()
-      vim.keymap.set('n', '<C-n>', ':Neotree filesystem toggle left<CR>', {})
-      vim.keymap.set('n', '<leader>bf', ':Neotree buffers reveal float<CR>', {})
-	end,
+      vim.keymap.set("n", "<C-n>", ":Neotree filesystem toggle left<CR>", {})
+      vim.keymap.set("n", "<leader>bf", ":Neotree buffers reveal float<CR>", {})
+    end,
   },
+
   {
-  "neovim/nvim-lspconfig",
-  dependencies = {
-    "williamboman/mason.nvim",
-    "williamboman/mason-lspconfig.nvim",
-    "hrsh7th/nvim-cmp",
-    "hrsh7th/cmp-nvim-lsp",
-    "hrsh7th/cmp-buffer",
-    "hrsh7th/cmp-path",
-  },
-  config = function()
-    -- Mason
-    require("mason").setup()
+    "neovim/nvim-lspconfig",
+    dependencies = {
+      "williamboman/mason.nvim",
+      "williamboman/mason-lspconfig.nvim",
+      "hrsh7th/nvim-cmp",
+      "hrsh7th/cmp-nvim-lsp",
+      "hrsh7th/cmp-buffer",
+      "hrsh7th/cmp-path",
+      "SmiteshP/nvim-navic", -- breadcrumb backend
+    },
+    config = function()
+      require("mason").setup()
 
-    require("mason-lspconfig").setup({
-      ensure_installed = {
-        "lua_ls",
-        "ts_ls",
-        "pyright",
-        "clangd",
-      },
-    })
+      require("mason-lspconfig").setup({
+        ensure_installed = {
+          "lua_ls",
+          "ts_ls",
+          "pyright",
+          "clangd",
+        },
+      })
 
-    -- nvim-cmp (completion UI)
-    local cmp = require("cmp")
+      local cmp = require("cmp")
+      local navic = require("nvim-navic")
 
-    cmp.setup({
-      mapping = cmp.mapping.preset.insert({
-        ["<CR>"] = cmp.mapping.confirm({ select = true }),
-        ["<Tab>"] = cmp.mapping.select_next_item(),
-        ["<S-Tab>"] = cmp.mapping.select_prev_item(),
-      }),
-      sources = {
-        { name = "nvim_lsp" },
-        { name = "buffer" },
-        { name = "path" },
-      },
-    })
+      cmp.setup({
+        mapping = cmp.mapping.preset.insert({
+          ["<CR>"] = cmp.mapping.confirm({ select = true }),
+          ["<Tab>"] = cmp.mapping.select_next_item(),
+          ["<S-Tab>"] = cmp.mapping.select_prev_item(),
+        }),
+        sources = {
+          { name = "nvim_lsp" },
+          { name = "buffer" },
+          { name = "path" },
+        },
+      })
 
-    -- LSP capabilities (connect LSP → cmp)
-    local capabilities = require("cmp_nvim_lsp").default_capabilities()
+      local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
-    -- Neovim 0.11+ native LSP config
-    vim.lsp.config("lua_ls", {
-      capabilities = capabilities,
-      settings = {
-        Lua = {
-          diagnostics = {
-            globals = { "vim" },
+      local on_attach = function(client, bufnr)
+        if client.server_capabilities.documentSymbolProvider then
+          navic.attach(client, bufnr)
+        end
+      end
+
+      vim.lsp.config("lua_ls", {
+        capabilities = capabilities,
+        on_attach = on_attach,
+        settings = {
+          Lua = {
+            diagnostics = {
+              globals = { "vim" },
+            },
           },
         },
-      },
-    })
+      })
 
-    vim.lsp.config("ts_ls", {
-      capabilities = capabilities,
-    })
+      vim.lsp.config("ts_ls", {
+        capabilities = capabilities,
+        on_attach = on_attach,
+      })
 
-    vim.lsp.config("pyright", {
-      capabilities = capabilities,
-    })
+      vim.lsp.config("pyright", {
+        capabilities = capabilities,
+        on_attach = on_attach,
+      })
 
-    vim.lsp.config("clangd", {
-      capabilities = capabilities,
-    })
-  end,
-},
+      vim.lsp.config("clangd", {
+        capabilities = capabilities,
+        on_attach = on_attach,
+      })
+    end,
+  },
+
   {
     "nvim-lualine/lualine.nvim",
     config = function()
-      require('lualine').setup({
+      require("lualine").setup({
         options = {
-          theme = 'dracula'
-        }
+          theme = "dracula",
+        },
       })
+    end,
+  },
 
-    end
-  }
-
+  -- =========================
+  -- Breadcrumbs (winbar)
+  -- =========================
+  {
+    "utilyre/barbecue.nvim",
+    name = "barbecue",
+    version = "*",
+    dependencies = {
+      "SmiteshP/nvim-navic",
+      "nvim-tree/nvim-web-devicons",
+    },
+    config = function()
+      require("barbecue").setup({
+        show_dirname = true,
+        show_basename = true,
+        show_modified = true,
+        symbols = {
+          separator = " › ",
+        },
+      })
+    end,
+  },
 }
 
 require("lazy").setup(plugins, {})
